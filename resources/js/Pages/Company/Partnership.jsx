c77f7142a293ddfc59c65ffc69ffffd078fe4e80
Rob Thomas <robt84@gmail.com>
2024-06-25 13:40:52 -0500

    added sections to the partnership page

--- resources/js/Pages/Company/Partnership.jsx
+++ resources/js/Pages/Company/Partnership.jsx
@@ -1,9 +1,284 @@
 import Marcom from "@/Layouts/MarcomLayout.jsx";
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import ClientLogos from "@/Components/ClientLogos.jsx";
+import {
+    ArrowPathIcon,
+    CloudArrowUpIcon,
+    Cog6ToothIcon,
+    FingerPrintIcon,
+    LockClosedIcon,
+    ServerIcon,
+} from '@heroicons/react/20/solid'
+import Testimonials from "@/Components/Testimonials.jsx";
 
+const stats = [
+    { label: 'Founded', value: '2021' },
+    { label: 'Employees', value: '37' },
+    { label: 'Countries', value: '12' },
+    { label: 'Raised', value: '$25M' },
+]
+
+const features = [
+    {
+        name: 'Push to deploy.',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+        icon: CloudArrowUpIcon,
+    },
+    {
+        name: 'SSL certificates.',
+        description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+        icon: LockClosedIcon,
+    },
+    {
+        name: 'Simple queues.',
+        description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.',
+        icon: ArrowPathIcon,
+    },
+    {
+        name: 'Advanced security.',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+        icon: FingerPrintIcon,
+    },
+    {
+        name: 'Powerful API.',
+        description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+        icon: Cog6ToothIcon,
+    },
+    {
+        name: 'Database backups.',
+        description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ',
+        icon: ServerIcon,
+    },
+]
 export default function Partnership() {
     return (
         <Marcom>
-            <h1>Partnership</h1>
+            <div className="relative isolate overflow-hidden bg-white">
+                <svg
+                    className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+                    aria-hidden="true"
+                >
+                    <defs>
+                        <pattern
+                            id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+                            width={200}
+                            height={200}
+                            x="50%"
+                            y={-1}
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <path d="M.5 200V.5H200" fill="none"/>
+                        </pattern>
+                    </defs>
+                    <rect width="100%" height="100%" strokeWidth={0} fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)"/>
+                </svg>
+                <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+                    <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
+                        <img
+                            className="h-11"
+                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                            alt="Your Company"
+                        />
+                        <div className="mt-24 sm:mt-32 lg:mt-16">
+                            <a href="#" className="inline-flex space-x-6">
+              <span
+                  className="rounded-full bg-indigo-600/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-600/10">
+                What's new
+              </span>
+                                <span
+                                    className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600">
+                <span>Just shipped v1.0</span>
+                <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+              </span>
+                            </a>
+                        </div>
+                        <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                            Deploy to the cloud with confidence
+                        </h1>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                            Elit sunt amet
+                            fugiat veniam occaecat fugiat aliqua.
+                        </p>
+                        <div className="mt-10 flex items-center gap-x-6">
+                            <a
+                                href="#"
+                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Get started
+                            </a>
+                            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                                Learn more <span aria-hidden="true">→</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div
+                        className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+                        <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+                            <div
+                                className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                                <img
+                                    src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+                                    alt="App screenshot"
+                                    width={2432}
+                                    height={1442}
+                                    className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <ClientLogos/>
+            <div className="bg-white py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div
+                        className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                        <div className="lg:pr-4">
+                            <div
+                                className="relative overflow-hidden rounded-3xl bg-gray-900 px-6 pb-9 pt-64 shadow-2xl sm:px-12 lg:max-w-lg lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
+                                <img
+                                    className="absolute inset-0 h-full w-full object-cover brightness-125 saturate-0"
+                                    src="https://images.unsplash.com/photo-1630569267625-157f8f9d1a7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80"
+                                    alt=""
+                                />
+                                <div className="absolute inset-0 bg-gray-900 mix-blend-multiply"/>
+                                <div
+                                    className="absolute left-1/2 top-1/2 -ml-16 -translate-x-1/2 -translate-y-1/2 transform-gpu blur-3xl"
+                                    aria-hidden="true"
+                                >
+                                    <div
+                                        className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-40"
+                                        style={{
+                                            clipPath:
+                                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                                        }}
+                                    />
+                                </div>
+                                <figure className="relative isolate">
+                                    <svg
+                                        viewBox="0 0 162 128"
+                                        fill="none"
+                                        aria-hidden="true"
+                                        className="absolute -left-2 -top-4 -z-10 h-32 stroke-white/20"
+                                    >
+                                        <path
+                                            id="0ef284b8-28c2-426e-9442-8655d393522e"
+                                            d="M65.5697 118.507L65.8918 118.89C68.9503 116.314 71.367 113.253 73.1386 109.71C74.9162 106.155 75.8027 102.28 75.8027 98.0919C75.8027 94.237 75.16 90.6155 73.8708 87.2314C72.5851 83.8565 70.8137 80.9533 68.553 78.5292C66.4529 76.1079 63.9476 74.2482 61.0407 72.9536C58.2795 71.4949 55.276 70.767 52.0386 70.767C48.9935 70.767 46.4686 71.1668 44.4872 71.9924L44.4799 71.9955L44.4726 71.9988C42.7101 72.7999 41.1035 73.6831 39.6544 74.6492C38.2407 75.5916 36.8279 76.455 35.4159 77.2394L35.4047 77.2457L35.3938 77.2525C34.2318 77.9787 32.6713 78.3634 30.6736 78.3634C29.0405 78.3634 27.5131 77.2868 26.1274 74.8257C24.7483 72.2185 24.0519 69.2166 24.0519 65.8071C24.0519 60.0311 25.3782 54.4081 28.0373 48.9335C30.703 43.4454 34.3114 38.345 38.8667 33.6325C43.5812 28.761 49.0045 24.5159 55.1389 20.8979C60.1667 18.0071 65.4966 15.6179 71.1291 13.7305C73.8626 12.8145 75.8027 10.2968 75.8027 7.38572C75.8027 3.6497 72.6341 0.62247 68.8814 1.1527C61.1635 2.2432 53.7398 4.41426 46.6119 7.66522C37.5369 11.6459 29.5729 17.0612 22.7236 23.9105C16.0322 30.6019 10.618 38.4859 6.47981 47.558L6.47976 47.558L6.47682 47.5647C2.4901 56.6544 0.5 66.6148 0.5 77.4391C0.5 84.2996 1.61702 90.7679 3.85425 96.8404L3.8558 96.8445C6.08991 102.749 9.12394 108.02 12.959 112.654L12.959 112.654L12.9646 112.661C16.8027 117.138 21.2829 120.739 26.4034 123.459L26.4033 123.459L26.4144 123.465C31.5505 126.033 37.0873 127.316 43.0178 127.316C47.5035 127.316 51.6783 126.595 55.5376 125.148L55.5376 125.148L55.5477 125.144C59.5516 123.542 63.0052 121.456 65.9019 118.881L65.5697 118.507Z"
+                                        />
+                                        <use href="#0ef284b8-28c2-426e-9442-8655d393522e" x={86}/>
+                                    </svg>
+                                    <img src="https://tailwindui.com/img/logos/workcation-logo-white.svg" alt=""
+                                         className="h-12 w-auto"/>
+                                    <blockquote className="mt-6 text-xl font-semibold leading-8 text-white">
+                                        <p>
+                                            “Amet amet eget scelerisque tellus sit neque faucibus non eleifend. Integer
+                                            eu praesent at a. Ornare
+                                            arcu gravida natoque erat et cursus tortor.”
+                                        </p>
+                                    </blockquote>
+                                    <figcaption className="mt-6 text-sm leading-6 text-gray-300">
+                                        <strong className="font-semibold text-white">Judith Rogers,</strong> CEO at
+                                        Workcation
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="text-base leading-7 text-gray-700 lg:max-w-lg">
+                                <p className="text-base font-semibold leading-7 text-indigo-600">Company values</p>
+                                <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                                    On a mission to empower remote teams
+                                </h1>
+                                <div className="max-w-xl">
+                                    <p className="mt-6">
+                                        Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis
+                                        mauris semper sed amet
+                                        vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra
+                                        tellus varius sit neque
+                                        erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+                                        enim. Mattis mauris
+                                        semper sed amet vitae sed turpis id.
+                                    </p>
+                                    <p className="mt-8">
+                                        Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id
+                                        blandit molestie
+                                        auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius
+                                        vulputate et ultrices
+                                        hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer
+                                        orci.
+                                    </p>
+                                    <p className="mt-8">
+                                        Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id
+                                        blandit molestie
+                                        auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius
+                                        vulputate et ultrices
+                                        hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer
+                                        orci.
+                                    </p>
+                                </div>
+                            </div>
+                            <dl className="mt-10 grid grid-cols-2 gap-8 border-t border-gray-900/10 pt-10 sm:grid-cols-4">
+                                {stats.map((stat, statIdx) => (
+                                    <div key={statIdx}>
+                                        <dt className="text-sm font-semibold leading-6 text-gray-600">{stat.label}</dt>
+                                        <dd className="mt-2 text-3xl font-bold leading-10 tracking-tight text-gray-900">{stat.value}</dd>
+                                    </div>
+                                ))}
+                            </dl>
+                            <div className="mt-10 flex">
+                                <a href="#" className="text-base font-semibold leading-7 text-indigo-600">
+                                    Learn more about our company <span aria-hidden="true">&rarr;</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-gray-900 py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto max-w-2xl sm:text-center">
+                        <h2 className="text-base font-semibold leading-7 text-indigo-400">Everything you need</h2>
+                        <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">No server? No
+                            problem.</p>
+                        <p className="mt-6 text-lg leading-8 text-gray-300">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
+                            suscipit eaque, iste
+                            dolor cupiditate blanditiis.
+                        </p>
+                    </div>
+                </div>
+                <div className="relative overflow-hidden pt-16">
+                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                        <img
+                            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                            alt="App screenshot"
+                            className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-white/10"
+                            width={2432}
+                            height={1442}
+                        />
+                        <div className="relative" aria-hidden="true">
+                            <div className="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-gray-900 pt-[7%]"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+                    <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+                        {features.map((feature) => (
+                            <div key={feature.name} className="relative pl-9">
+                                <dt className="inline font-semibold text-white">
+                                    <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-500"
+                                                  aria-hidden="true"/>
+                                    {feature.name}
+                                </dt>
+                                {' '}
+                                <dd className="inline">{feature.description}</dd>
+                            </div>
+                        ))}
+                    </dl>
+                </div>
+            </div>
+            <Testimonials/>
         </Marcom>
     );
 }
