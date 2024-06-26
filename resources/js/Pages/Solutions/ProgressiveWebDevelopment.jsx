0b31303e71e68dc0b72653920bbd27e644242511
Rob Thomas <robt84@gmail.com>
2024-06-26 12:27:09 -0500

    Update ProgressiveWebDevelopment.jsx

--- resources/js/Pages/Solutions/ProgressiveWebDevelopment.jsx
+++ resources/js/Pages/Solutions/ProgressiveWebDevelopment.jsx
@@ -43,7 +43,7 @@ export default function ProgressiveWebDevelopment() {
         <Marcom>
             <div className="bg-gradient-to-b from-[#101212] relative to-[#08201D]">
                 <div className="relative lg:min-h-[1000px] pt-24 pb-10 sm:pt-32 sm:pb-16 lg:pb-24">
-                    <div className="absolute inset-x-0 bottom-0 z-10 hidden lg:flex">
+                    <div className="absolute inset-x-0 bottom-0 hidden lg:flex">
                         <img className="hidden w-full lg:block"
                              src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/5/credit-cards.png"
                              alt=""/>
@@ -52,7 +52,7 @@ export default function ProgressiveWebDevelopment() {
                              alt=""/>
                     </div>
 
-                    <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 relative z-20">
+                    <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 relative">
                         <div className="max-w-xl mx-auto text-center">
                             <h1 className="text-4xl font-bold sm:text-6xl">
                                 <span
